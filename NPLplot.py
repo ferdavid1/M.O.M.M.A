f3ff36aadacc4b7b678349f7a8d5a8a6def6eb39
Samir Akre <sakre@ucdavis.edu>
2017-04-29 20:59:46 -0700

    COLORS also no michigan

--- NPLplot.py
+++ NPLplot.py
@@ -17,15 +17,14 @@ US_general = data[3]
 stateDens = data[4:len(data)]
 
 
-print([d for d in data if d[0] == 'Arkansas']) #example of how to index by name.
 
 states_names=['Washington', 'Wisconsin', 'West Virginia', 'Florida', 'Wyoming', 'New Hampshire', 'New Jersey', 'New Mexico', 'National', 'North Carolina', 'North Dakota', 'Nebraska', 'New York', 'Rhode Island', 'Nevada', 'Guam', 'Colorado', 'California', 'Georgia', 'Connecticut', 'Oklahoma', 'Ohio', 'Kansas', 'South Carolina', 'Kentucky', 'Oregon', 'South Dakota', 'Delaware', 'District of Columbia', 'Hawaii', 'Puerto Rico', 'Texas', 'Louisiana', 'Tennessee', 'Pennsylvania', 'Virginia', 'Virgin Islands', 'Alaska', 'Alabama', 'American Samoa', 'Arkansas', 'Vermont', 'Illinois', 'Indiana', 'Iowa', 'Arizona', 'Idaho', 'Maine', 'Maryland', 'Massachusetts', 'Utah', 'Missouri', 'Minnesota', 'Michigan', 'Montana', 'Northern Mariana Islands', 'Mississippi']
 data = pd.read_csv('dataSets/NPL.csv', usecols=[2,3]).get_values()
-print data[0]
+
 
 fig = plt.figure()
 
-blcLat=25.309974
+blcLat=20.309974
 blcLon=-128.068124
 trcLat=49.592734
 trcLon=-51.315958
@@ -38,12 +37,12 @@ m.drawparallels(np.arange(-60.,90.,30.),labels=[1,0,0,0])
 
 m.drawcountries()
 
-m.readshapefile('st99_d00', name='states', drawbounds=True)
+m.readshapefile('st99_d00', name='states', drawbounds=False)
 
 # collect the state names from the shapefile attributes so we can
 # look up the shape obect for a state by it's name
 
-
+cm= plt.get_cmap('YlOrBr')
 ax = plt.gca() # get current axes instance
 states_names = []
 dens=[x[4] for x in stateDens]
@@ -62,13 +61,15 @@ for state in states_names:
 	if stateD:
 		stateD=stateD[0]
 		seg = m.states[states_names.index(state)]
-		poly = Polygon(seg, facecolor=str(1-stateD[-1]),zorder=0)
+		poly = Polygon(seg, facecolor=cm(stateD[-1]),zorder=0)
 		ax.add_patch(poly)
+
+
 
 
 lats=[i[0] for i in data]
 lons=[i[1] for i in data]
 
 #CS = m.hexbin(lats,lons,latlon=True,C=z,gridsize=bins,cmap=plt.cm.jet)
-m.scatter(lons,lats,latlon=True,zorder=2,s=1, color="red")
+m.scatter(lons,lats,latlon=True,zorder=2,s=1, color="black")
 plt.show()
