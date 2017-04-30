970fb9406e5ed8b9d830cf77aa0e4f8d45b075cd
ferdavid1 <fespinosa@ucdavis.edu>
2017-04-29 18:53:19 -0700

    aaa

--- explore.py
+++ explore.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 data = pd.read_csv('dataSets/pop_density.csv', usecols=[0,10,11,21,22,32,33])
 
-data = pd.DataFrame(data)
+data = data.get_values()
+data = [list(d) for d in data]
 
-print(data[0])
+US_general = data[3]
+States = data[4:len(data)]
+
+print(States[3])
+print(US_general)
+
+print([d for d in data if d[0] == 'Arkansas']) #example of how to index by name.
